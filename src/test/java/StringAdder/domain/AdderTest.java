8a482e07932c0965f129d9e8e803750a72af9665
pjhsk113 <pjhsk113@naver.com>
2021-01-18 20:20:28 +0900

    [add] Empty_List Test 추가

--- src/test/java/StringAdder/domain/AdderTest.java
+++ src/test/java/StringAdder/domain/AdderTest.java
@@ -1,11 +1,14 @@
 package StringAdder.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -30,4 +33,10 @@ class AdderTest {
         assertThat(result).isSameAs(expected);
     }
 
+    @DisplayName("빈 값이 들어왔을 경우 0을 리턴하는지 테스트")
+    @Test
+    void empty_test() {
+        List<Integer> input = Collections.emptyList();
+        assertThat(Adder.add(input)).isEqualTo(0);
+    }
 }
