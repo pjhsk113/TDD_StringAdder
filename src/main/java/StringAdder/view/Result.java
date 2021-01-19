587c4fdfecae4cc6b5f997a57ae832e54b7f56fc
pjhsk113 <pjhsk113@naver.com>
2021-01-19 23:24:38 +0900

    [feat] 결과를 출력하는 Result 클래스 추가

---
+++ src/main/java/StringAdder/view/Result.java
@@ -0,0 +1,9 @@
+package StringAdder.view;
+
+public class Result {
+    private static final String ARROW = "=> ";
+
+    public static void printResult(int result) {
+        System.out.println(ARROW + result);
+    }
+}
