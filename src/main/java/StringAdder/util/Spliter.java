4bca2c9ca72d40ffd472a5fa1299bd98d73e854e
pjhsk113 <pjhsk113@naver.com>
2021-01-20 20:17:30 +0900

    [refactor] 매직넘버 상수화

--- src/main/java/StringAdder/util/Spliter.java
+++ src/main/java/StringAdder/util/Spliter.java
@@ -3,6 +3,9 @@ package StringAdder.util;
 import java.util.List;
 
 public class Spliter {
+    private static final int CUSTOM_DELIMITER_START_INDEX = 2;
+    private static final int CUSTOM_DELIMITER_END_INDEX = 3;
+    private static final int CUSTOM_NUMBER_START_INDEX = 5;
     private static final String DELIMITER_PATTERN = "[,:]";
 
     private Spliter() { }
@@ -18,11 +21,11 @@ public class Spliter {
     }
 
     private static String subStringDelimiter(String input) {
-        return input.substring(2, 3);
+        return input.substring(CUSTOM_DELIMITER_START_INDEX, CUSTOM_DELIMITER_END_INDEX);
     }
 
     private static String subStringFormula(String input) {
-        return input.substring(5);
+        return input.substring(CUSTOM_NUMBER_START_INDEX);
     }
 
 }
