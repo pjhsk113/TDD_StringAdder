f0ef711ee4e5326373c5dcfe996cbbc18ec6458e
pjhsk113 <pjhsk113@naver.com>
2021-01-20 20:16:51 +0900

    [refactor] StringAdderExceptionHandler 클래스 이름 변경
    - StringAdderExceptionHandler -> ConverterExceptionHandler로 변경
    - Converter쪽에서 역할을 나누어주는 느낌이라 변경함.

--- src/main/java/StringAdder/exception/ConverterExceptionHandler.java
+++ src/main/java/StringAdder/exception/ConverterExceptionHandler.java
@@ -1,9 +1,9 @@
 package StringAdder.exception;
 
-public class StringAdderExceptionHandler {
+public class ConverterExceptionHandler {
     private static final String CAN_NOT_INPUT_NEGATIVE= "음수는 입력할 수 없습니다!";
 
-    private StringAdderExceptionHandler() {}
+    private ConverterExceptionHandler() {}
 
     public static void validate(int input) {
         isPositive(input);
